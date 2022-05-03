0e3d373dc1415470f9d572971b11f1c3ce07ec43
Shourav Rajbongshi <mrinmoyraj823@gmail.com>
2022-05-03 13:57:04 +0600

    nav bar added

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import Navigation from '../Shared/Navigation/Navigation';
 const Home = () => {
     return (
         <div>
-            <h2>This is home</h2>
+            <Navigation> </Navigation>
             <img src="https://thumbs.gfycat.com/ConventionalOblongFairybluebird-max-1mb.gif" alt="" />
+            
         </div>
     );
 };
