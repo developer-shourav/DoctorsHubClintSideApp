242254714712bcc1b18e814deb4e4e4a3de323f2
Shourav Rajbongshi <mrinmoyraj823@gmail.com>
2022-05-03 15:52:34 +0600

    image and service added

---
+++ src/components/Pages/Home/Service/Service.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Service = () => {
+    return (
+        <div>
+            <h1>This is singel service </h1>
+        </div>
+    );
+};
+
+export default Service;
