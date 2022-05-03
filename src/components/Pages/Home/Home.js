905c4eb4fbf1fae6544815249983d6344321d79f
Shourav Rajbongshi <mrinmoyraj823@gmail.com>
2022-05-03 14:05:15 +0600

    services component added

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navigation from '../Shared/Navigation/Navigation';
+import Services from './Services/Services';
 const Home = () => {
     return (
         <div>
             <Navigation> </Navigation>
             <img src="https://thumbs.gfycat.com/ConventionalOblongFairybluebird-max-1mb.gif" alt="" />
             
+            <Services> </Services>
         </div>
     );
 };
---
+++ src/components/Pages/Home/Services/Services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            <h2>This is Services</h2>
+        </div>
+    );
+};
+
+export default Services;
