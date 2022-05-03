68d19a48633e82ba9d5f9a872e95f7d06cb96e40
Shourav Rajbongshi <mrinmoyraj823@gmail.com>
2022-05-03 16:52:29 +0600

    card added

--- src/components/Pages/Home/Service/Service.js
+++ src/components/Pages/Home/Service/Service.js
@@ -3,28 +3,29 @@ import React from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
-
-
-
 const Service = (props) => {
-    const {name, description, img} = props.service;
+  const { name, description, img } = props.service;
   return (
     <Grid item xs={2} sm={4} md={4}>
       <Card sx={{ minWidth: 275 }}>
+      <CardMedia
+            component="img"
+            style={{width:'auto', height:"80px", margin:"20px auto 5px auto"}}
+            image={img}
+            alt="Dental Service"
+          />
         <CardContent>
-           <img src={img} alt="" />
+         
+          
           <Typography variant="h5" component="div">
             {name}
           </Typography>
-         
-          <Typography variant="body2">
-            {description}
-            <br />
-            {'"a benevolent smile"'}
-          </Typography>
+
+          <Typography variant="body2">{description}</Typography>
         </CardContent>
         <CardActions>
           <Button size="small">Learn More</Button>
