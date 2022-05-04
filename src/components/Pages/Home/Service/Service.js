8fad58e68e9d92294d0a1d33c4422a4991b5b069
Shourav Rajbongshi <mrinmoyraj823@gmail.com>
2022-05-04 19:10:22 +0600

    first commit of wednesday

--- src/components/Pages/Home/Service/Service.js
+++ src/components/Pages/Home/Service/Service.js
@@ -1,17 +1,15 @@
 import { Grid } from "@mui/material";
 import React from "react";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
 const Service = (props) => {
   const { name, description, img } = props.service;
   return (
     <Grid item xs={2} sm={4} md={4}>
-      <Card sx={{ minWidth: 275 }}>
+      <Card sx={{ minWidth: 275, border:0, boxShadow: 0 }}>
       <CardMedia
             component="img"
             style={{width:'auto', height:"80px", margin:"20px auto 5px auto"}}
@@ -25,11 +23,9 @@ const Service = (props) => {
             {name}
           </Typography>
 
-          <Typography variant="body2">{description}</Typography>
+          <Typography variant="body2" color="text.secondary">{description}</Typography>
         </CardContent>
-        <CardActions>
-          <Button size="small">Learn More</Button>
-        </CardActions>
+        
       </Card>
     </Grid>
   );
