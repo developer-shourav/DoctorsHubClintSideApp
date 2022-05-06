f2bd98ef1ae696f7014259e3cb33529ff03c3855
Shourav Rajbongshi <mrinmoyraj823@gmail.com>
2022-05-06 20:01:33 +0600

    appointment section developmetn added

--- src/components/Pages/Home/AppiontmentBanner/AppiontmentBanner.js
+++ src/components/Pages/Home/AppiontmentBanner/AppiontmentBanner.js
@@ -3,20 +3,27 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import doctor from "../../../../images/doctor.png";
 import bg from "../../../../images/appointment-bg.png";
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 
-const appointmentBg = {
+const appointmentBanner = {
   background: `url(${bg})`,
+  backgroundColor: "rgba(45, 58, 74, .85)",
+  backgroundBlendMode:"darken, luminosity",
+  marginTop: "175px"
 };
 const AppiontmentBanner = () => {
   return (
-    <Box sx={{ flexGrow: 1 }} style={appointmentBg}>
+    <Box sx={{ flexGrow: 1 }} style={appointmentBanner}>
       <Grid container spacing={2}>
         <Grid item xs={12} md={6}>
-          <img src={doctor} style={{ width: "400px" }} alt="DoctorsPhoto" />
+          <img src={doctor} style={{ width: "400px", marginTop:"-110px" }} alt="DoctorsPhoto" />
         </Grid>
         <Grid item xs={12} md={6}>
-          <Typography variant="h6">Appointment</Typography>
+          <Typography style={{color:"#19D3B0"}} variant="h6">Appointment</Typography>
+
+          <Typography  style={{color:"#fff"}} variant="h4">Make an Appointment Today</Typography>
+          <Typography   style={{color:"#fff", fontSize:"18px", fontWeight:"300", textAlign:"left"}} variant="h6">It is a long established fact thatareoder will be distractedby the readable content ofapage when looking at its.</Typography>
+          <Button variant="contained"> Learn More</Button>
         </Grid>
       </Grid>
     </Box>
