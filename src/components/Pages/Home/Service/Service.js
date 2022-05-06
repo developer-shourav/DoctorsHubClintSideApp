8f44827870527b79877b9785651be624a92ac37b
Shourav Rajbongshi <mrinmoyraj823@gmail.com>
2022-05-06 17:23:21 +0600

    code redution for preventing conde conflicts

--- src/components/Pages/Home/Service/Service.js
+++ src/components/Pages/Home/Service/Service.js
@@ -10,28 +10,7 @@ import Typography from "@mui/material/Typography";
 const Service = (props) => {
   const { name, description, img } = props.service;
   return (
-    <Grid item xs={2} sm={4} md={4}>
-      <Card sx={{ minWidth: 275 }}>
-      <CardMedia
-            component="img"
-            style={{width:'auto', height:"80px", margin:"15px auto 5px auto"}}
-            image={img}
-            alt="Dental Service"
-          />
-        <CardContent>
-         
-          
-          <Typography variant="h5" component="div">
-            {name}
-          </Typography>
-
-          <Typography variant="body2">{description}</Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card>
-    </Grid>
+    
   );
 };
 
